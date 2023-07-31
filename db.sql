4ccd32e244dcde690f2a94937edb63b37786c032
hovien2201 <124018097+hovien2201@users.noreply.github.com>
2023-07-31 10:38:12 +0700

    vb

--- db.sql
+++ db.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE clothing_store;
-USE clothing_store;
+CREATE DATABASE venco_fan;
+USE venco_fan;
 	
 CREATE TABLE roles(
 id BIGINT PRIMARY KEY AUTO_INCREMENT,
@@ -26,10 +26,7 @@ id BIGINT PRIMARY KEY AUTO_INCREMENT,
 img VARCHAR(200) UNIQUE NOT NULL
 );
 
-CREATE TABLE colors(
-id BIGINT PRIMARY KEY AUTO_INCREMENT,
-color VARCHAR(10) UNIQUE NOT NULL
-);
+
 
 
 CREATE TABLE products(
@@ -42,7 +39,6 @@ descrip VARCHAR(1000),
 create_date DATETIME DEFAULT CURRENT_TIMESTAMP,
 update_date DATETIME,
 type_product BIGINT NOT NULL,FOREIGN KEY(type_product) REFERENCES type_product(id),
-color_id BIGINT NOT NULL,FOREIGN KEY(color_id) REFERENCES colors(id),
 img_id BIGINT NOT NULL,FOREIGN KEY(img_id) REFERENCES img_product(id)
 );
 
@@ -62,30 +58,12 @@ create_date DATETIME DEFAULT CURRENT_TIMESTAMP,
 update_date DATETIME
 );
 
-create table employees(
-id BIGINT PRIMARY KEY AUTO_INCREMENT,
-user_id BIGINT NOT NULL,
-FOREIGN KEY(user_id) REFERENCES users(id),
-`name` varchar(255) not null,
-birthday varchar(50) not null,
-gender varchar(10) not null,
-email varchar(255) not null UNIQUE,
-phone_number varchar(11) NOT NULL  UNIQUE ,
-address varchar(255) not null,
-salary BIGINT not null CHECK (salary > 0),
-image mediumtext,
-is_delete BIT DEFAULT 0,
-create_date DATETIME DEFAULT CURRENT_TIMESTAMP,
-update_date DATETIME
-);
-
 CREATE TABLE bill_detail(
 id BIGINT PRIMARY KEY AUTO_INCREMENT,
 quantity INT NOT NULL,
 product_id BIGINT NOT NULL,FOREIGN KEY(product_id) REFERENCES products(id),
 price BIGINT NOT NULL,
-customer_id BIGINT NOT NULL,FOREIGN KEY(customer_id) REFERENCES customers(id),
-employee_id BIGINT NOT NULL,FOREIGN KEY(employee_id) REFERENCES employees(id)
+customer_id BIGINT NOT NULL,FOREIGN KEY(customer_id) REFERENCES customers(id)
 );
 
 CREATE TABLE bills(
@@ -103,4 +81,11 @@ customer_id BIGINT NOT NULL,FOREIGN KEY(customer_id) REFERENCES customers(id),
 rating INT NOT NULL	,
 comments VARCHAR(1000),
 create_date DATETIME DEFAULT CURRENT_TIMESTAMP
-)
+);
+
+CREATE TABLE shoping_cart(
+id BIGINT PRIMARY KEY AUTO_INCREMENT,
+product_id BIGINT NOT NULL,FOREIGN KEY(product_id) REFERENCES products(id),
+quantity INT NOT NULL,
+customer_id BIGINT NOT NULL,FOREIGN KEY(customer_id) REFERENCES customers(id)
+);
